c307ae7aa29244e1e610a2b8c38be0b863ed3ad7
Van Hoang <nbdev82@gmail.com>
2023-05-30 14:02:31 +0700

    current user and finalreport

---
+++ CitizenManagement_EntityFramework/DTO/CurrentUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitizenManagement_EntityFramework
+{
+    public class CurrentUser
+    {
+        private static CurrentUser instance;
+        private Cityzen currentCitizen;
+        private Accounts currentAccount;
+
+        public static CurrentUser Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new CurrentUser();
+                return instance;
+            }
+        }
+
+        public Cityzen CurrentCitizen { get => currentCitizen; set => currentCitizen = value; }
+        public Accounts CurrentAccount { get => currentAccount; set => currentAccount = value; }
+
+
+    }
+}
