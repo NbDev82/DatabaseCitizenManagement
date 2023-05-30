10ac29d004df3e51ccda9bdd9703030c173417d3
cong09112003 <congnguyenhuu2703@gmail.com>
2023-05-30 14:57:30 +0700

    clone

---
+++ CitizenManagement_EntityFramework/DAO/People_MarriageDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitizenManagement_EntityFramework.DAO
+{
+    public class People_MarriageDAO
+    {
+        public DataTable DataGridPeopleMarriage()
+        {
+            string sqlStr = string.Format("SELECT * FROM V_GetPeopleMarriage");
+            return DBConnection.Instance.GetDataTable(sqlStr);
+        }
+        public int CountData
+    }
+}
